923cd126591c747578fd4c57d264e3b35632e31e
Timi Petre <gaxxis@yahoo.com>
2021-09-08 19:56:56 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# listdetail_cloudfirestore
+ 
---
+++ android/app/src/main/kotlin/com/example/listdetail_cloudfirestore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.listdetail_cloudfirestore
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
